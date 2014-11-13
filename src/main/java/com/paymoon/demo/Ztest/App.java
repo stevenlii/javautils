3263314195e707b10157617db204acf49994b53e
stevenlii <stevenlii@qq.com>
2014-11-13 23:39:00 +0800

    ttt

---
+++ src/main/java/com/paymoon/demo/Ztest/App.java
@@ -0,0 +1,13 @@
+package com.paymoon.demo.Ztest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "A Hello World!" );
+    }
+}
---
+++ src/main/java/com/paymoon/demo/Ztest/Test.java
@@ -0,0 +1,7 @@
+package com.paymoon.demo.Ztest;
+
+public class Test {
+public static void main(String[] args) {
+	System.out.println("fuck!!!");
+}
+}
