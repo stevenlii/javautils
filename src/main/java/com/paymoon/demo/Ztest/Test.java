eed9846966629d6df15fe836c13d61df655925fb
stevenlii <stevenlii@qq.com>
2014-11-20 09:45:50 +0800

    test change

--- src/main/java/com/paymoon/demo/Ztest/Test.java
+++ src/main/java/com/paymoon/demo/Ztest/Test.java
@@ -1,5 +1,9 @@
 package com.paymoon.demo.Ztest;
 
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -7,9 +11,17 @@ import org.apache.logging.log4j.Logger;
 public class Test {
 	private static final Logger logger = LogManager.getLogger(); 
 public static void main(String[] args) {
-	String msg = new String("妹的!!!");
-	System.out.println(msg);
+	String msg = new String("abcdef");
+	msg = msg.substring(1);
+	Map<String, String> map1 = new HashMap<String, String>();
+	Map<String, String> map2 = new HashMap<String, String>();
+	map2.put("1", "2");
+	map1.putAll(map2);
+	
+	System.out.println(map1.get("1"));
 	logger.info(msg);
+	logger.info(msg,new Test().getClazzName());
+	
 }
 /**
  * @author Administrator
