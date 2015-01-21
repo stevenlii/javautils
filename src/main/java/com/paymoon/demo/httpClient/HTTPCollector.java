b8dc2ee7fec85b4af6ed3a5f944ef14bc585c0b9
stevenlii <stevenlii@qq.com>
2015-01-21 10:56:22 +0800

    增加Http监控功能示例, via httpclient [httpcore httpmime httpclient]

---
+++ src/main/java/com/paymoon/demo/httpClient/HTTPCollector.java
@@ -0,0 +1,8 @@
+package com.paymoon.demo.httpClient;
+
+public class HTTPCollector {
+	public static void main(String[] args) {
+		HttpClient httpClient = new HttpClient();
+		httpClient.postForm();
+	}
+}
