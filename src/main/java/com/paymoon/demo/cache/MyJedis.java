627daedc83afd7f8e1849d10971bd65550a103b5
lizhiqiang09 <lizhiqiang09@meicai.cn>
2019-07-11 20:50:59 +0800

    tag2 commit

--- src/main/java/com/paymoon/demo/cache/MyJedis.java
+++ src/main/java/com/paymoon/demo/cache/MyJedis.java
@@ -10,7 +10,7 @@ public class MyJedis {
 	public static void main(String[] args) {
 		//just ip without port:6379
 		Jedis jedis = new Jedis("101.227.255.236",6379);
-		jedis.set("foo", "bar122");
+		jedis.set("foo", "bar");
 		String value = jedis.get("foo");
 		System.out.println(value);
 		jedis.set("foo", "foo");
